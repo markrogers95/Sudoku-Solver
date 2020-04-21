d785f28c32f2ef03bb12b4b04a73ba02c8bb4857
markrogers95 <mark_199518@hotmail.co.uk>
2020-04-21 21:08:19 +0100

    CLA flags

--- SUDargs.c
+++ SUDargs.c
@@ -10,33 +10,58 @@
 
 #include "SUDhead.h"
 
-void flagSet(char arg[], mode_t *mode){
-
-    for (int i = 1; i < strlen(arg); i++){
-        
-        if (arg[i] == 'c'){
-            printf("Triggering check flag!\n");
-            mode->check=1;
-        }
-        else if (arg[i] == 'f'){
-            printf("Triggering file output!\n");
-            mode->file=1;
-        }
-        else printf("Not recognized\n");
-    }
-    printf("%d", mode->file);
-    return;
-}
-
 void argValidate (int argc, char *argv[], mode_t *mode ){
 
-    for (int i = 1; i < argc + 1; i++)
-    {
-        if ( argv[i][0] == '-')
-        {
-            flagSet(argv[i], mode);
-        }
-        else printf("%s is not a flag!\n", argv[i]);
+    printf("Checking arguments.. ");
+    if (argc < 2){
+        printf("no CLIs passed. Returning to main in default mode.\n");
+        return;
     }
+    printf("\n");
+    for (int i = 2; i < argc + 1; i++){
+
+        if ( argv[i - 1][0] == '-'){
+
+            for (int j = 1; j < strlen(argv[i-1]); j++){
+
+                if (argv[i-1][j] == 'c'){
+                    
+                    if (mode->check != 1){
+                        printf("Check flag acknowledged..\n");
+                        mode->check=1;
+                    }
+                }
+                else if (argv[i-1][j] == 'f'){
+                    
+                    if (mode->file != 1){
+                        
+                        printf("File output acknowledged..\n");
+                        mode->file=1;
+                    }
+                }
+                else{
+                    printf("\n-%c is not a valid command line", argv[i-1][j]);
+                    printf(" argument. It will be ignored.\n");
+                }
+            }
+        }
+        else printf("%s is a FILE to be verified.\n", argv[i-1]);
+    }
+    printf("Finished validating arguments..");
+
+    if (mode->check == 0 && mode->file == 1){
+        
+        printf("returning to main in FILEOUT mode.\n");
+    }
+    else if (mode->file == 0 && mode->check == 1){
+        
+        printf("returning to main in CHECK mode.\n");
+    }
+    else if (mode->check == 1 && mode->file ==1){
+        
+        printf("returning to main in CHECK mode with FILEOUT on.\n");
+    }
+    else printf("returning to main in DEFAULT mode.");
+
     return;
 }
--- SUDmain.c
+++ SUDmain.c
@@ -20,36 +20,10 @@ void printSud(int arr[n][n]);
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    {
-        printf("\nNo arguments supplied. Program will run in solve ");
-        printf("mode and output to screen, subject to user input.\n");
-        printf("Do you wish to continue? ");
-        printf ("[1] Continue.");
-        printf ("[2] Exit. ");
-
-        int cli = 0;
-
-        while(cli != 1)
-        {
-            scanf ("%d", &cli);
-
-            if (cli == 2)
-            {
-                printf(".. exiting.\n");
-                return 0;
-            }
-            else if (cli == 1)
-            {
-                printf(".. continuing.\n");
-                cli = 1;
-            }
-            else
-            {
-                printf("You must press 1 or 2.\n");
-            }
-        }
-    }
+    mode_t flags = {0, 0};
+    
+    argValidate(argc, argv, &flags);
+    printf("\nFile output: %d\n", flags.file);
 
     int sud[n][n] = {{0,0,0,8,2,0,0,6,0},
                      {0,9,1,0,0,0,0,4,0},
